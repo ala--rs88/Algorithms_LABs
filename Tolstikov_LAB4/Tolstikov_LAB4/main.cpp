9e199deeaf3a8e752f04066ac7bbf53bf834d826
Igor Karpov <igork.fet@gmail.com>
2015-11-13 13:45:10 +0300

    + Empty project for LAB4 added.

---
+++ Tolstikov_LAB4/Tolstikov_LAB4/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Tolstikov_LAB4
+//
+//  Created by Igor Karpov on 11/13/15.
+//  Copyright © 2015 Igor Karpov. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
