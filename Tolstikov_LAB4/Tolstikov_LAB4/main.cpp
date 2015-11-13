78a1fa4f98afdae13be33c6d9313cf593f7ef3ad
Igor Karpov <igork.fet@gmail.com>
2015-11-13 20:43:15 +0300

    + LAB4: reads from disk speed test implemented.

--- Tolstikov_LAB4/Tolstikov_LAB4/main.cpp
+++ Tolstikov_LAB4/Tolstikov_LAB4/main.cpp
@@ -9,14 +9,19 @@
 #include <iostream>
 #include <fstream>
 
+#define TEST_RUNS_COUNT 10
+
 void generateLargeRandomFile(const char *filePath)
 {
+    std::cout << "Large random file gererating started. File path: " << filePath << std::endl;
+    const auto startTime = std::clock();
+    
     std::ofstream outfile (filePath,std::ofstream::binary);
 
     long bufferSize = 1024;
     char* buffer = new char[bufferSize];
     
-    for (int i = 0; i < 1024 * 100; i++)
+    for (int i = 0; i < 1024 * 1024; i++)
     {
         for (int j = 0; j < bufferSize; j++)
         {
@@ -27,12 +32,58 @@ void generateLargeRandomFile(const char *filePath)
     
     delete[] buffer;
     outfile.close();
+    
+    const auto endTime = std::clock();
+    std::cout << "Large random file gererating finised. Took " << double(endTime - startTime) / CLOCKS_PER_SEC << "s" << std::endl << std::endl;
+}
+
+double getReadFromDiskSpeed(const char *filePath)
+{
+    std::ifstream infile (filePath,std::ifstream::binary);
+    
+    long bufferSize = 1024 * 1024 * 1024;
+    char* buffer = new char[bufferSize];
+    std::streamsize bytesRead = 0;
+    
+    const auto startTime = std::clock();
+    
+    while(infile.read(buffer, bufferSize))
+    {
+        bytesRead += infile.gcount();
+    }
+    
+    const auto endTime = std::clock();
+    
+    delete[] buffer;
+    infile.close();
+    
+    double totalTimeInSecs = double(endTime - startTime) / CLOCKS_PER_SEC;
+    double speed = (bytesRead / (1024*1024)) / totalTimeInSecs;
+    
+    return speed;
+}
+
+void testReadFromDiskSpeed(const char *filePath)
+{
+    std::cout << "Read from disk speed test started. File path: " << filePath << std::endl;
+    
+    double sumSpeed = 0;
+    for (int i = 0; i < TEST_RUNS_COUNT; i++)
+    {
+        double speed = getReadFromDiskSpeed(filePath);
+        sumSpeed += speed;
+        std::cout << speed << std::endl;
+    }
+    double averageSpeed = sumSpeed / TEST_RUNS_COUNT;
+    
+    std::cout << "Read from disk speed test finised. Test runs count: " << TEST_RUNS_COUNT << ". Average read speed: " << averageSpeed << " megabytes/sec" << std::endl << std::endl;
 }
 
 int main(int argc, const char *argv[])
 {
     const char *filePath = "large_file.txt";
-    std::cout << "File gererating started: " << filePath << std::endl << std::endl;
     generateLargeRandomFile(filePath);
+    testReadFromDiskSpeed(filePath);
+    
     return 0;
 }
