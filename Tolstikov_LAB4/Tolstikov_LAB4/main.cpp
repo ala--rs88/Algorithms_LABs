e8c22dabf2a3c64f8f7755f29a6754d573eb3802
Igor Karpov <igork.fet@gmail.com>
2015-11-19 19:32:23 +0300

    + LAB4: random access time & random read speed measurements added.

--- Tolstikov_LAB4/Tolstikov_LAB4/main.cpp
+++ Tolstikov_LAB4/Tolstikov_LAB4/main.cpp
@@ -73,14 +73,113 @@ void testReadFromDiskSpeed(const char *filePath)
     }
     double averageSpeed = sumSpeed / TEST_RUNS_COUNT;
     
-    std::cout << "Read from disk speed test finised. Test runs count: " << TEST_RUNS_COUNT << ". Average read speed: " << averageSpeed << " megabytes/sec" << std::endl << std::endl;
+    std::cout << "Read from disk speed test finised. Test runs count: " << TEST_RUNS_COUNT << std::endl << ". Average read speed: " << averageSpeed << " megabytes/sec" << std::endl << std::endl;
 }
+
+
+double getAccessTimeToDisk(const char *filePath)
+{
+    int randomReadsCount = 1000;
+    int readGroupSize = (1024*1024*1024 / randomReadsCount);
+    
+    long bufferSize = 1;
+    char* buffer = new char[bufferSize];
+    
+    std::ifstream infile (filePath,std::ifstream::binary);
+    
+    const auto startTime = std::clock();
+    for (int i = 0; i < randomReadsCount; i++)
+    {
+        int alreadySeeked = i * readGroupSize;
+        int seekInCurrentGroup = rand() % (readGroupSize - 1);
+        
+        infile.seekg(alreadySeeked + seekInCurrentGroup, std::ios::beg);
+        infile.read(buffer, bufferSize);
+    }
+    const auto endTime = std::clock();
+    
+    delete[] buffer;
+    infile.close();
+    
+    double totalTimeInMillisecs = (double(endTime - startTime) / CLOCKS_PER_SEC) * 1000;
+    double accessTime = totalTimeInMillisecs / randomReadsCount;
+
+    return accessTime;
+}
+
+void testAccessTimeToDisk(const char *filePath)
+{
+    std::cout << "Access time to disk test started. File path: " << filePath << std::endl;
+    
+    double sumSpeed = 0;
+    for (int i = 0; i < TEST_RUNS_COUNT; i++)
+    {
+        double speed = getAccessTimeToDisk(filePath);
+        sumSpeed += speed;
+        std::cout << speed << std::endl;
+    }
+    double averageSpeed = sumSpeed / TEST_RUNS_COUNT;
+    
+    std::cout << "Access time to disk test finised. Test runs count: " << TEST_RUNS_COUNT << std::endl << ". Average access time: " << averageSpeed << " millisec" << std::endl << std::endl;
+}
+
+double getAccessToDiskSpeed(const char *filePath)
+{
+    std::streamsize bytesRead = 0;
+    //int randomReadsCount = 1000;
+    //int readGroupSize = (1024*1024*1024 / randomReadsCount);
+    
+    long bufferSize = 1;
+    char* buffer = new char[bufferSize];
+    
+    std::ifstream infile (filePath,std::ifstream::binary);
+    
+    const auto startTime = std::clock();
+    while (bytesRead < 1024)
+    {
+        int seekPosition = rand() % (1024*1024*1024 - 1000);
+        
+        infile.seekg(seekPosition, std::ios::beg);
+        infile.read(buffer, bufferSize);
+        bytesRead += bufferSize;
+    }
+    const auto endTime = std::clock();
+    
+    delete[] buffer;
+    infile.close();
+    
+    double totalTimeInSecs = double(endTime - startTime) / CLOCKS_PER_SEC;
+    double speed = ((double)bytesRead / (1024*1024)) / totalTimeInSecs;
+    
+    return speed;
+}
+
+void testAccessToDiskSpeed(const char *filePath)
+{
+    std::cout << "Access to disk speed test started. File path: " << filePath << std::endl;
+    
+    double sumSpeed = 0;
+    for (int i = 0; i < TEST_RUNS_COUNT; i++)
+    {
+        double speed = getAccessToDiskSpeed(filePath);
+        sumSpeed += speed;
+        std::cout << speed << std::endl;
+    }
+    double averageSpeed = sumSpeed / TEST_RUNS_COUNT;
+    
+    std::cout << "Access to disk speed test finised. Test runs count: " << TEST_RUNS_COUNT << std::endl << ". Average access speed: " << averageSpeed << " megabytes/sec" << std::endl << std::endl;
+}
+
 
 int main(int argc, const char *argv[])
 {
-    const char *filePath = "large_file.txt";
-    generateLargeRandomFile(filePath);
+    const char *filePath = "/Users/IgorKarpov/Documents/BSU_II/large_file_XXX.txt";
+    //generateLargeRandomFile(filePath);
     testReadFromDiskSpeed(filePath);
+    std::cout << " --------------------------------  --------------------------------  -------------------------------- " << std::endl;
+    testAccessTimeToDisk(filePath);
+    std::cout << " --------------------------------  --------------------------------  -------------------------------- " << std::endl;
+    testAccessToDiskSpeed(filePath);
     
     return 0;
 }
